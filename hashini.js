3a4312dd6a1a4f38ccd908a4056086925401c09c
Hashini Tharuka <150510405+HashiniTharuka@users.noreply.github.com>
2026-04-25 22:35:12 +0530

    updated feature X

---
+++ hashini.js
@@ -0,0 +1,19 @@
+// hashini.js
+
+// Simple program
+console.log("Hello Hashini! 👋");
+
+// Function example
+function greet(name) {
+    return `Hi ${name}, welcome to JavaScript!`;
+}
+
+// Call function
+console.log(greet("Hashini"));
+
+// Simple calculation
+function add(a, b) {
+    return a + b;
+}
+
+console.log("Sum:", add(5, 3));
